2858486747df2abac1e70d866de49c9797c2622c
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-04 16:55:10 +0100

    implementación componente service con cliente HTTP RestTemplate

--- Microservicios/Curso Udemy MicroServicios/springboot-servicio-item/src/main/java/com/proyectosPersonales/springboot/app/item/models/service/ItemServiceImpl.java
+++ Microservicios/Curso Udemy MicroServicios/springboot-servicio-item/src/main/java/com/proyectosPersonales/springboot/app/item/models/service/ItemServiceImpl.java
@@ -1,29 +1,39 @@
 package com.proyectosPersonales.springboot.app.item.models.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.proyectosPersonales.springboot.app.item.models.Item;
+import com.proyectosPersonales.springboot.app.item.models.Producto;
 
 @Service
 public class ItemServiceImpl implements ItemService {
 
 	@Autowired
-	private RestTemplate clienteRestprobando;
+	private RestTemplate clienteRest;
 	
 	@Override
 	public List<Item> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8001/listar", Producto[].class ));
+		
+		return productos.stream()
+				.map(producto -> Item.builder().producto(producto).cantidad(1).build())
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public Item findById(Long id, Integer cantidad) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", id.toString());
+		Producto producto = clienteRest.getForObject("http://localhost:8001/listar/{id}", Producto.class, pathVariables);
+		return Item.builder().producto(producto).cantidad(1).build();
 	}
 
 }
