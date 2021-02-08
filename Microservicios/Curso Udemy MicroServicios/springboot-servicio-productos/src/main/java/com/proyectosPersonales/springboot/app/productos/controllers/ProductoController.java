310e2b73259c02dcbdbe25d60355b894abb0a0f2
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-08 12:03:14 +0100

    se añade especificación sobre curso microservicios

--- Microservicios/Curso Udemy MicroServicios/springboot-servicio-productos/src/main/java/com/proyectosPersonales/springboot/app/productos/controllers/ProductoController.java
+++ Microservicios/Curso Udemy MicroServicios/springboot-servicio-productos/src/main/java/com/proyectosPersonales/springboot/app/productos/controllers/ProductoController.java
@@ -15,38 +15,38 @@ import com.proyectosPersonales.springboot.app.productos.models.service.IProducto
 
 @RestController
 public class ProductoController {
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Value("${server.port}")
 	private Integer port;
-	
+
 	@Autowired
 	private IProductoService productoService;
-	
+
 	@GetMapping("/listar")
 	public List<Producto> listar() {
-		return productoService.findAll().stream()
-				.map(producto -> {
-					//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-					producto.setPort(port);
-					return producto;
-				})
-				.collect(Collectors.toList());
+		return productoService.findAll().stream().map(producto -> {
+			// producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
+			return producto;
+		}).collect(Collectors.toList());
 	}
-	
+
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
 		Producto producto = productoService.findById(id);
-		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-		
+		// producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+
 		producto.setPort(port);
 
-		/*
-		 * try { Thread.sleep(2000L); } catch (InterruptedException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); }
-		 */
+		try {
+			Thread.sleep(2000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		return producto;
 	}
 
