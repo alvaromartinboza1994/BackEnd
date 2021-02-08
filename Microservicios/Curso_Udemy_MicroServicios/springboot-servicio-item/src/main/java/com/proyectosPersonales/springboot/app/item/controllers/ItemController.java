d3ae8dad57bc3073581710f5aaeb69f5a84fb04c
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-08 17:08:12 +0100

    se actualizan as configuraciones con @RefreshScope y Actuator

--- Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-item/src/main/java/com/proyectosPersonales/springboot/app/item/controllers/ItemController.java
+++ Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-item/src/main/java/com/proyectosPersonales/springboot/app/item/controllers/ItemController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import com.proyectosPersonales.springboot.app.item.models.service.ItemService;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RefreshScope
 @RestController
 @Slf4j
 public class ItemController {
