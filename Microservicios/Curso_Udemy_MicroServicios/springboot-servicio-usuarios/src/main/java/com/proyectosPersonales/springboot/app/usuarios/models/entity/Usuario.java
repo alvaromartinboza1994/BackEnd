6bc1bdfbe4a842388fee5c762daa55c162eb6a35
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-10 09:06:47 +0100

    se crea la relacion de muchos a muchos entre Usuario y Rol

--- Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios/src/main/java/com/proyectosPersonales/springboot/app/usuarios/models/entity/Usuario.java
+++ Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios/src/main/java/com/proyectosPersonales/springboot/app/usuarios/models/entity/Usuario.java
@@ -1,13 +1,19 @@
 package com.proyectosPersonales.springboot.app.usuarios.models.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 
@@ -33,4 +39,9 @@ public class Usuario implements Serializable {
 	@Column(unique = true, length = 100)
 	private String email;
 
+	@ManyToMany(fetch = FetchType.LAZY) //a medida que se le llamen a los get, se realizan las consultas a la BBDD
+	@JoinTable(name="usuarios_roles", joinColumns = @JoinColumn(name="usuario_id"),
+		inverseJoinColumns = @JoinColumn(name="rol_id"), uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "rol_id"})}) 
+	private List<Rol> roles;//se crea usuarios_roles
+	
 }
