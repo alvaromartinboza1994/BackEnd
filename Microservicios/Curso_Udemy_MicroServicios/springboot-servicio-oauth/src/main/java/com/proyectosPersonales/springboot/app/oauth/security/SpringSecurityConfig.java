4662f13a8a5f35d586890d11ab4a6c19ba076166
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-10 12:49:37 +0100

    se añade la clase SpringSecurityConfig y registro UserDetailsService

---
+++ Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-oauth/src/main/java/com/proyectosPersonales/springboot/app/oauth/security/SpringSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.proyectosPersonales.springboot.app.oauth.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UserDetailsService usuarioService;//inyecta el componente UsuarioService anotado con @Service
+
+	@Override
+	@Autowired //se inyecta el objeto AuthenticationManagerBuilder
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());//encripta password
+	}
+
+	@Bean //para que se guarde en el contenedor de spring y lo podamos usar para encriptar las contraseñas
+	private BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
+	
+	
+}
