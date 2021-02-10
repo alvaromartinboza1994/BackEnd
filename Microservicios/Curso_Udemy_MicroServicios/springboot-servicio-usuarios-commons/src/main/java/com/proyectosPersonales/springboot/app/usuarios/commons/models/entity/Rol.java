92b3c7ab2920f9f2a037f2d957611ef8a5abdb54
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-10 12:41:00 +0100

    se implementa servicio UserDetailsService con Feign Client

--- Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios-commons/src/main/java/com/proyectosPersonales/springboot/app/usuarios/commons/models/entity/Rol.java
+++ Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios-commons/src/main/java/com/proyectosPersonales/springboot/app/usuarios/commons/models/entity/Rol.java
@@ -1,6 +1,7 @@
 package com.proyectosPersonales.springboot.app.usuarios.commons.models.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,9 +10,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "roles")
 public class Rol implements Serializable {
--- Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios-commons/src/main/java/com/proyectosPersonales/springboot/app/usuarios/commons/models/entity/Usuario.java
+++ Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios-commons/src/main/java/com/proyectosPersonales/springboot/app/usuarios/commons/models/entity/Usuario.java
@@ -15,11 +15,15 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "usuarios")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario implements Serializable {
 
 	@Id
