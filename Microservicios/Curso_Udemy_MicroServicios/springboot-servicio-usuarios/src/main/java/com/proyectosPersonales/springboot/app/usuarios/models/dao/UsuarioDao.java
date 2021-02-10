c1cd324227f8a34ca86cf4f4f430e9171d3e0ae1
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-10 10:02:52 +0100

    exportar métodos repositorio rest con endpoint search

--- Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios/src/main/java/com/proyectosPersonales/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios/src/main/java/com/proyectosPersonales/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -2,14 +2,17 @@ package com.proyectosPersonales.springboot.app.usuarios.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.proyectosPersonales.springboot.app.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path="usuarios") //exporta el CRUD repository a un endpoint de nuestra API REST. En path le indicamos el endpoint donde vamos a usar el crud repository
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{ //PagindAndSorting hereda de CrudRepository <clase, tipo clave primaria>
 //además permite ordenar y otras características
-	public Usuario findByUsername(String username); 
+	@RestResource(path="buscarUsername") //ruta alternativa al nombre del método
+	public Usuario findByUsername(@Param("username") String username); 
 
 	@Query("select u from Usuario u where u.username =?1") //and u.email=?2
 	public Usuario obtenerPorUsername(String username /*, String email*/);
