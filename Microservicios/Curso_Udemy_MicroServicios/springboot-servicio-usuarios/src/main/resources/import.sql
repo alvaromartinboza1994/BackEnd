28dafef95da91f79c4d69cfbb7d20df6bc1b400a
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-11 11:10:18 +0100

    se añade configuracion para postgreSQL

--- Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios/src/main/resources/import.sql
+++ Microservicios/Curso_Udemy_MicroServicios/springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Alvaro', '$2a$10$HzAzVt4KEY8I1.2i9z9Bxe4BVcKdEH5WBKDqIN0upmYlxWJ3JIC5S', 1, 'Alvaro', 'Martin', 'alvaro@gmail.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Administrador', '$2a$10$6iJ.lmsiqkGJjU24iHLCheZzWYikzSiSy1pKRIL0AWPh64MlTOWZi', 1, 'Paco', 'Fernandez', 'paco@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Alvaro', '$2a$10$HzAzVt4KEY8I1.2i9z9Bxe4BVcKdEH5WBKDqIN0upmYlxWJ3JIC5S', true, 'Alvaro', 'Martin', 'alvaro@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Administrador', '$2a$10$6iJ.lmsiqkGJjU24iHLCheZzWYikzSiSy1pKRIL0AWPh64MlTOWZi', true, 'Paco', 'Fernandez', 'paco@gmail.com');
 
 INSERT INTO roles (nombre) VALUES ('ROL_USER');
 INSERT INTO roles (nombre) VALUES ('ROL_ADMIN');
