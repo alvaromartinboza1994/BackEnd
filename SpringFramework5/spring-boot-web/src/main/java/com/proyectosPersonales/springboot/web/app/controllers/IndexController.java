f08dc5f33b4a67ddffc5583faf0530baa965da7d
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-16 13:01:45 +0100

    uso de objeto Model para mostrar datos en las vistas

--- SpringFramework5/spring-boot-web/src/main/java/com/proyectosPersonales/springboot/web/app/controllers/IndexController.java
+++ SpringFramework5/spring-boot-web/src/main/java/com/proyectosPersonales/springboot/web/app/controllers/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.proyectosPersonales.springboot.web.app.models.Usuario;
+
 @Controller //componente de spring de tipo controlador. Toda clase anotada con component, será un objeto manejado por el contenedor de spring framework automaticamente
 //maneja las consultas del usuario
 @RequestMapping("/app")//ruta de primer nivel generica para todos los metodos del controlador
@@ -14,5 +16,13 @@ public class IndexController {
 	public String index(Model model) {//model usa un mapa de java para asignar valores mediante llave y valor. Tambien vale con ModelMap, Map<String, Object> -> put y ModelAndView -> addObject (return ModelAndView)
 		model.addAttribute("titulo", "hola Spring Framework!");
 		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = Usuario.builder().nombre("Alvaro").apellido("Martin").build();
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
 	}
 }
---
+++ SpringFramework5/spring-boot-web/src/main/java/com/proyectosPersonales/springboot/web/app/models/Usuario.java
@@ -0,0 +1,13 @@
+package com.proyectosPersonales.springboot.web.app.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Usuario {
+	
+	private String nombre;
+	private String apellido;
+
+}
