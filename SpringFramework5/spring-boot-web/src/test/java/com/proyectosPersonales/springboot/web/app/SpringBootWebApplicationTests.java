4662bddd77634e0d8d5eda8be0ffe7e55dcc2884
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-16 11:36:14 +0100

    base para proyecto web con Spring Boot

---
+++ SpringFramework5/spring-boot-web/src/test/java/com/proyectosPersonales/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectosPersonales.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
