a2bc89942b5c2b98c59f1446732b659aeb1e5c58
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-16 12:32:02 +0100

    cómo pasar datos a la vista

--- SpringFramework5/spring-boot-web/src/main/java/com/proyectosPersonales/springboot/web/app/controllers/IndexController.java
+++ SpringFramework5/spring-boot-web/src/main/java/com/proyectosPersonales/springboot/web/app/controllers/IndexController.java
@@ -1,16 +1,16 @@
 package com.proyectosPersonales.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller //componente de spring de tipo controlador. Toda clase anotada con component, será un objeto manejado por el contenedor de spring framework automaticamente
 //maneja las consultas del usuario
 public class IndexController { 
 
 	@GetMapping({"/index" , "/", "/home"})//tres rutas posibles para este metodo
-	public String index() {
+	public String index(Model model) {//model usa un mapa de java para asignar valores mediante llave y valor. Tambien vale con ModelMap, Map<String, Object> -> put y ModelAndView -> addObject (return ModelAndView)
+		model.addAttribute("titulo", "hola Spring Framework!");
 		return "index";
 	}
 }
