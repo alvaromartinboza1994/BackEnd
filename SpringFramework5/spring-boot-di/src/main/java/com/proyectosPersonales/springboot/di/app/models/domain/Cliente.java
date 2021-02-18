381c808093bac0b8b0f414d769d6738666991a17
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-18 11:26:00 +0100

    contextos (scope) de los componentes

--- SpringFramework5/spring-boot-di/src/main/java/com/proyectosPersonales/springboot/di/app/models/domain/Cliente.java
+++ SpringFramework5/spring-boot-di/src/main/java/com/proyectosPersonales/springboot/di/app/models/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.proyectosPersonales.springboot.di.app.models.domain;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import lombok.Data;
 
 @Data
 @Component
+@RequestScope
 public class Cliente {
 
 	@Value("${cliente.nombre}")
--- SpringFramework5/spring-boot-di/src/main/java/com/proyectosPersonales/springboot/di/app/models/domain/Factura.java
+++ SpringFramework5/spring-boot-di/src/main/java/com/proyectosPersonales/springboot/di/app/models/domain/Factura.java
@@ -1,5 +1,6 @@
 package com.proyectosPersonales.springboot.di.app.models.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -8,12 +9,21 @@ import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import lombok.Data;
 
 @Data
 @Component
-public class Factura {
+@RequestScope //este bean durara lo que dure una peticion http del usuario
+//@SessionScope //marca objeto como sesion -> carro de compra, sistema autenticacion.Persistente en la sesion http.Se destruye cuando cerremos el navegador, cuando ocurra un timeout o se invalide la sesion
+//@ApplicationScope //o singleton
+public class Factura implements Serializable{//si es sesion debe de ser serializable
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6995825952807717735L;//identificador de la serializacion
 
 	@Value("${factura.descripcion}")
 	private String descripcion;
@@ -32,6 +42,7 @@ public class Factura {
 	}
 	
 	//se ejecuta al parar el servidor del servicio
+	//tras poner scope, se destruye cuando finaliza la peticion http
 	@PreDestroy
 	public void destruir() {
 		System.out.println("Factura destruida".concat(descripcion));
