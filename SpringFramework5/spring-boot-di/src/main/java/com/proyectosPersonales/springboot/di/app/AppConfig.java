ec3fc426efa17748454fa5e8d5457d9664342c63
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-18 10:55:38 +0100

    se registran mas lineas para la factura

--- SpringFramework5/spring-boot-di/src/main/java/com/proyectosPersonales/springboot/di/app/AppConfig.java
+++ SpringFramework5/spring-boot-di/src/main/java/com/proyectosPersonales/springboot/di/app/AppConfig.java
@@ -38,5 +38,21 @@ public class AppConfig {
 		
 		return Arrays.asList(linea1, linea2);
 	}
+	
+	@Bean("itemsFacturaEquipamiento")
+	@Primary
+	public List<ItemFactura> registrarItemsOficina() {
+		Producto producto1 = Producto.builder().nombre("Bolsa de deporte Acliclas").precio(15).build();
+		Producto producto2 = Producto.builder().nombre("Chandal Mike").precio(50).build();
+		Producto producto3 = Producto.builder().nombre("Gorra Kampion").precio(20).build();
+		Producto producto4 = Producto.builder().nombre("Sudadera Jeebok").precio(40).build();
+		
+		ItemFactura linea1 = ItemFactura.builder().producto(producto1).cantidad(2).build();
+		ItemFactura linea2 = ItemFactura.builder().producto(producto2).cantidad(4).build();
+		ItemFactura linea3 = ItemFactura.builder().producto(producto3).cantidad(1).build();
+		ItemFactura linea4 = ItemFactura.builder().producto(producto4).cantidad(3).build();
+		
+		return Arrays.asList(linea1, linea2, linea3, linea4);
+	}
 
 }
--- SpringFramework5/spring-boot-di/src/main/java/com/proyectosPersonales/springboot/di/app/models/domain/Factura.java
+++ SpringFramework5/spring-boot-di/src/main/java/com/proyectosPersonales/springboot/di/app/models/domain/Factura.java
@@ -3,6 +3,7 @@ package com.proyectosPersonales.springboot.di.app.models.domain;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,7 @@ public class Factura {
 	private Cliente cliente;
 
 	@Autowired
+	//@Qualifier("itemsFacturaEquipamiento")
 	private List<ItemFactura> items;
 
 }
