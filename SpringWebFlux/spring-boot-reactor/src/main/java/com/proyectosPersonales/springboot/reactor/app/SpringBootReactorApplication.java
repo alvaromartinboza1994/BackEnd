777383b7d6ae366b4ee5b2ebda0ca809d2fbd590
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-19 11:43:05 +0100

    se hace uso de metodo subscribe

--- SpringWebFlux/spring-boot-reactor/src/main/java/com/proyectosPersonales/springboot/reactor/app/SpringBootReactorApplication.java
+++ SpringWebFlux/spring-boot-reactor/src/main/java/com/proyectosPersonales/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,32 @@
 package com.proyectosPersonales.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class SpringBootReactorApplication {
+@Slf4j
+public class SpringBootReactorApplication implements CommandLineRunner{ //para que la aplicacion sea de consola o lineas de comando tiene que implementar esta interfaz
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//creacion del primer observable
+		Flux<String> nombres = Flux.just("Alvaro", "Reme", "Jose Joaquin", "" , "Tania", "Carmen")
+				.doOnNext(elemento -> {
+					if(elemento.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden ser vacíos"); //manejo de errors
+					} else {
+						System.out.println(elemento);
+					}
+				}); //publiser = observable. cada vez que se emita un elemento, se imprime en pantalla
+		nombres.subscribe(log::info, error -> log.error(error.getMessage()));//sin esto no se imprime. 
 	}
 
 }
