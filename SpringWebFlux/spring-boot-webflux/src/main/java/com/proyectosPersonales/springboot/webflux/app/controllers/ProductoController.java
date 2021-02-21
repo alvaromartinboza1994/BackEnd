58ac3efe72bdd5533d066748baef43dc6592ec64
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-21 16:25:07 +0100

    se suscribe otro observador y se modifica el stream reactivo

--- SpringWebFlux/spring-boot-webflux/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
+++ SpringWebFlux/spring-boot-webflux/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.proyectosPersonales.springboot.webflux.app.models.dao.ProductoDao;
 import com.proyectosPersonales.springboot.webflux.app.models.documents.Producto;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 @Controller
+@Slf4j
 public class ProductoController {
 
 	@Autowired
@@ -18,7 +20,12 @@ public class ProductoController {
 	
 	@GetMapping({"/listar", "/"})
 	public String listar(Model model) {
-		Flux<Producto> productos = dao.findAll();
+		Flux<Producto> productos = dao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				});
+		productos.subscribe(producto -> log.info(producto.getNombre()));
 		
 		model.addAttribute("productos", productos);
 		model.addAttribute("titulo", "Listado de productos");
