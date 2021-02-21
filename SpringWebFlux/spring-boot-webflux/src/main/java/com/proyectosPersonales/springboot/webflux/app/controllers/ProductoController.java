b2f6cf771036d672c0385503cfbbdc1660669359
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-21 19:47:27 +0100

    se añade otra forma del editar

--- SpringWebFlux/spring-boot-webflux/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
+++ SpringWebFlux/spring-boot-webflux/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.proyectosPersonales.springboot.webflux.app.models.documents.Producto;
@@ -16,9 +19,11 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@SessionAttributes("producto") // cada vez que se pase en el crear o en el editar se pase a la vista se guarda
+								// en la sesion http
 @Controller
 @Slf4j
-public class ProductoController {//NO USAMOS EL SUSCRIPTOR EN EL CONTROLADOR
+public class ProductoController {// NO USAMOS EL SUSCRIPTOR EN EL CONTROLADOR
 
 	@Autowired
 	private ProductoService service;
@@ -34,15 +39,47 @@ public class ProductoController {//NO USAMOS EL SUSCRIPTOR EN EL CONTROLADOR
 		return Mono.just("listar");
 	}
 
-	@GetMapping("/form")
-	public Mono<String> crear(Model model) {//para rellenar mono producto en formulario
-		model.addAttribute("producto", new Producto());
-		model.addAttribute("titulo", "formulario de producto");
+	@GetMapping("/form/{id}")
+	public Mono<String> editar(@PathVariable String id, Model model) {// model lo usamos para pasar el objeto a la vista
+		Mono<Producto> productoMono = service.findById(id).doOnNext(p -> log.info("Producto: " + p.getNombre()))
+				.defaultIfEmpty(new Producto());
+		model.addAttribute("boton", "Editar");
+		model.addAttribute("titulo", "Editar Producto");
+		model.addAttribute("producto", productoMono);
+
 		return Mono.just("form");
 	}
-	
+
+	@GetMapping("/form-v2/{id}")
+	public Mono<String> editarV2(@PathVariable String id, Model model) {// model lo usamos para pasar el objeto a la //
+																		// vista
+		return service.findById(id).doOnNext(p -> {
+			log.info("Producto: " + p.getNombre());
+			model.addAttribute("boton", "Editar");
+			model.addAttribute("titulo", "Editar Producto");
+			model.addAttribute("producto", p);
+		}).defaultIfEmpty(new Producto())
+				.flatMap(p -> {
+					if(p.getId() == null) {
+						return Mono.error(new InterruptedException("No existe el producto"));
+					}
+					return Mono.just(p);
+				})
+				.then(Mono.just("form"))
+				.onErrorResume(ex -> Mono.just("redirect:/listar?error=no+existe+el+producto"));
+	}
+
+	@GetMapping("/form")
+	public Mono<String> crear(Model model) {// para rellenar mono producto en formulario
+		model.addAttribute("producto", new Producto());
+		model.addAttribute("titulo", "Formulario de producto");
+		model.addAttribute("boton", "Crear");
+		return Mono.just("form");
+	}
+
 	@PostMapping("/form")
-	public Mono<String> guardar(Producto producto) {
+	public Mono<String> guardar(Producto producto, SessionStatus status) {
+		status.setComplete();
 		return service.save(producto)
 				.doOnNext(p -> log.info("producto almacenado: " + p.getNombre() + " Id: " + p.getId()))
 				.thenReturn("redirect:/listar");
--- SpringWebFlux/spring-boot-webflux/src/main/java/com/proyectosPersonales/springboot/webflux/app/models/documents/Producto.java
+++ SpringWebFlux/spring-boot-webflux/src/main/java/com/proyectosPersonales/springboot/webflux/app/models/documents/Producto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +23,7 @@ public class Producto {
 	
 	private String nombre;
 	private Double precio;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 	public Producto(String nombre, Double precio) {
 		super();
