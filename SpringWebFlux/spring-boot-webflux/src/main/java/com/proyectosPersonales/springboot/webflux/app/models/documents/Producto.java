45ede730788ba04ad2b0bfd049918c76c9487868
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-21 14:42:58 +0100

    se agregan las clases del modelo document y repository

---
+++ SpringWebFlux/spring-boot-webflux/src/main/java/com/proyectosPersonales/springboot/webflux/app/models/documents/Producto.java
@@ -0,0 +1,21 @@
+package com.proyectosPersonales.springboot.webflux.app.models.documents;
+
+import java.sql.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "productos")
+public class Producto {
+	
+	@Id
+	private String id;
+	
+	private String nombre;
+	private Double precio;
+	private Date createAt;
+	
+}
