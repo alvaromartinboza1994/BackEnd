c53cd6448d65311f5a0d15ed3772faaca3c25950
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-22 10:41:44 +0100

    implementando handler GET para listar en RestController

---
+++ SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
@@ -0,0 +1,31 @@
+package com.proyectosPersonales.springboot.webflux.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyectosPersonales.springboot.webflux.app.models.documents.Producto;
+import com.proyectosPersonales.springboot.webflux.app.models.service.ProductoService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController //lo que devuelve cada metodo handler se guarda en el cuerpo de la respuesta y se publica en un json por defecto
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+	@Autowired
+	private ProductoService service;
+	
+	@GetMapping
+	public Mono<ResponseEntity< Flux<Producto>>> listar() {
+		return Mono.just(
+				ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(service.findAll())
+				);
+	}
+}
