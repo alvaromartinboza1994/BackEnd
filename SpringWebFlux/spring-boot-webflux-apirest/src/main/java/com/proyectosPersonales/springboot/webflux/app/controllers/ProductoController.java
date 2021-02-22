85d051cddf7d4275c26d8b46da5857b830473e78
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-22 10:52:41 +0100

    se implementa handler GET ver detalle en RestController

--- SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
+++ SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +28,14 @@ public class ProductoController {
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.body(service.findAll())
 				);
+	}
+	
+	@GetMapping("/{id}")
+	public Mono<ResponseEntity<Producto>> ver(@PathVariable String id) {
+		return service.findById(id)
+				.map(p -> ResponseEntity.ok()
+						.contentType(MediaType.APPLICATION_JSON_UTF8)
+						.body(p))
+						.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 }
