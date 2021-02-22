bde1dccddfc60d34ddad7a7940786bb8a546874c
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-22 14:13:36 +0100

    fin seccion

--- SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/handler/ProductoHandler.java
+++ SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/handler/ProductoHandler.java
@@ -2,11 +2,15 @@ package com.proyectosPersonales.springboot.webflux.app.handler;
 
 import static org.springframework.web.reactive.function.BodyInserters.*;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -21,6 +25,27 @@ public class ProductoHandler {
 
 	@Autowired
 	private ProductoService service;
+	
+	@Value("${config.uploads.path}")
+	private String path;
+
+	public Mono<ServerResponse> upload(ServerRequest request) {
+		String id = request.pathVariable("id");
+		return request.multipartData()
+				.map(multipart -> multipart.toSingleValueMap().get("file"))
+				.cast(FilePart.class)
+				.flatMap(filepart -> service.findById(id)
+						.flatMap(p -> {
+					p.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+					.replace(" ", "-")
+					.replace(":", "")
+					.replace("\\", ""));
+					return file.transferTo(new File(path + p.getFoto())).then(service.save(p));
+				})).flatmap(p -> ServerResponse.created(URI.create("/api/v2/productos".concat(p.getId())))
+						.contentType(MediaType.APPLICATION_JSON_UTF8)
+						.body(fromObject(p)))
+				.switchIfEmpty(ServerResponse.notFound().build());
+	}
 
 	public Mono<ServerResponse> listar(ServerRequest request) {
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(service.findAll(), Producto.class);
@@ -70,11 +95,11 @@ public class ProductoHandler {
 				.contentType(MediaType.APPLICATION_JSON_UTF8).body(service.save(p), Producto.class))
 				.switchIfEmpty(ServerResponse.notFound().build());
 	}
-	
+
 	public Mono<ServerResponse> eliminar(ServerRequest request) {
 		String id = request.pathVariable("id");
 		Mono<Producto> productoDb = service.findById(id);
-		
+
 		return productoDb.flatMap(p -> service.delete(p).then(ServerResponse.noContent().build()))
 				.switchIfEmpty(ServerResponse.notFound().build());
 	}
