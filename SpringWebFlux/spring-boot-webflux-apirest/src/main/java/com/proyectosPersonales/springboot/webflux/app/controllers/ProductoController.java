cb1aeff7f3f05cc0c5a96cf382c6e517f44f44a6
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-22 11:26:18 +0100

    se implementa handler upload de imagen

--- SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
+++ SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/controllers/ProductoController.java
@@ -1,12 +1,16 @@
 package com.proyectosPersonales.springboot.webflux.app.controllers;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.proyectosPersonales.springboot.webflux.app.models.documents.Producto;
@@ -22,63 +27,63 @@ import com.proyectosPersonales.springboot.webflux.app.models.service.ProductoSer
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RestController //lo que devuelve cada metodo handler se guarda en el cuerpo de la respuesta y se publica en un json por defecto
+@RestController // lo que devuelve cada metodo handler se guarda en el cuerpo de la respuesta y
+				// se publica en un json por defecto
 @RequestMapping("/api/productos")
 public class ProductoController {
 
 	@Autowired
 	private ProductoService service;
-	
-	@GetMapping
-	public Mono<ResponseEntity< Flux<Producto>>> listar() {
-		return Mono.just(
-				ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.body(service.findAll())
-				);
+
+	@Value("${config.uploads.path}")
+	private String path;
+
+	@PostMapping("/upload/{id}")
+	public Mono<ResponseEntity<Producto>> upload(@PathVariable String id, @RequestPart FilePart file) {
+		return service.findById(id).flatMap(p -> {
+			p.setFoto(UUID.randomUUID().toString() + "-"
+					+ file.filename().replace(" ", "").replace(":", "").replace("\\", ""));
+
+			return file.transferTo(new File(path + p.getFoto())).then(service.save(p));
+		}).map(p -> ResponseEntity.ok(p)).defaultIfEmpty(ResponseEntity.notFound().build());
 	}
-	
+
+	@GetMapping
+	public Mono<ResponseEntity<Flux<Producto>>> listar() {
+		return Mono.just(ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(service.findAll()));
+	}
+
 	@GetMapping("/{id}")
 	public Mono<ResponseEntity<Producto>> ver(@PathVariable String id) {
-		return service.findById(id)
-				.map(p -> ResponseEntity.ok()
-						.contentType(MediaType.APPLICATION_JSON_UTF8)
-						.body(p))
-						.defaultIfEmpty(ResponseEntity.notFound().build());
-	}
-	
-	@PostMapping
-	public Mono<ResponseEntity<Producto>> crear(@RequestBody Producto producto) {
-		if(producto.getCreateAt()==null) {
-			producto.setCreateAt(new Date());
-		}
-		return service.save(producto).map(p -> ResponseEntity
-				.created(URI.create("/api/productos/".concat(p.getId())))
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.body(p)
-				);
-	}
-	
-	@PutMapping("/{id}")
-	public Mono<ResponseEntity<Producto>> editar(@RequestBody Producto producto, @PathVariable String id) {
-		return service.findById(id)
-				.flatMap(p -> {
-					p.setNombre(producto.getNombre());
-					p.setPrecio(producto.getPrecio());
-					p.setCategoria(producto.getCategoria());
-					return service.save(p);
-				})
-				.map(p -> ResponseEntity.created(URI.create("/api/productos".concat(p.getId())))
-						.contentType(MediaType.APPLICATION_JSON_UTF8)
-						.body(p))
+		return service.findById(id).map(p -> ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(p))
 				.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
-	
+
+	@PostMapping
+	public Mono<ResponseEntity<Producto>> crear(@RequestBody Producto producto) {
+		if (producto.getCreateAt() == null) {
+			producto.setCreateAt(new Date());
+		}
+		return service.save(producto).map(p -> ResponseEntity.created(URI.create("/api/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON_UTF8).body(p));
+	}
+
+	@PutMapping("/{id}")
+	public Mono<ResponseEntity<Producto>> editar(@RequestBody Producto producto, @PathVariable String id) {
+		return service.findById(id).flatMap(p -> {
+			p.setNombre(producto.getNombre());
+			p.setPrecio(producto.getPrecio());
+			p.setCategoria(producto.getCategoria());
+			return service.save(p);
+		}).map(p -> ResponseEntity.created(URI.create("/api/productos".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON_UTF8).body(p))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
 	@DeleteMapping("/{id}")
 	public Mono<ResponseEntity<Void>> eliminar(@PathVariable String id) {
-		return service.findById(id)
-				.flatMap(p -> {
-					return service.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
-				}).defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
+		return service.findById(id).flatMap(p -> {
+			return service.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
+		}).defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
 	}
 }
