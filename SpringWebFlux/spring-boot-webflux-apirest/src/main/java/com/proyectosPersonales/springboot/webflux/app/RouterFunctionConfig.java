b94dffffeeb4730d5ee18391ba4696cc3467dd5c
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-02-22 13:28:46 +0100

    se implementa handler PUT para actualizar

--- SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/RouterFunctionConfig.java
+++ SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/RouterFunctionConfig.java
@@ -21,6 +21,7 @@ public class RouterFunctionConfig {
 		return route(GET("/api/v2/productos").or(GET("/api/v3/productos")),
 				/*request ->*/handler::listar)
 				.andRoute(GET("/api/v2/productos/{id}"), handler::ver)
-				.andRoute(POST("/api/v2/productos"), handler::crear);
+				.andRoute(POST("/api/v2/productos"), handler::crear)
+				.andRoute(PUT("/api/v2/productos/{id}"), handler::editar);
 	}
 }
--- SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/handler/ProductoHandler.java
+++ SpringWebFlux/spring-boot-webflux-apirest/src/main/java/com/proyectosPersonales/springboot/webflux/app/handler/ProductoHandler.java
@@ -18,37 +18,56 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class ProductoHandler {
-	
+
 	@Autowired
 	private ProductoService service;
 
 	public Mono<ServerResponse> listar(ServerRequest request) {
-		return ServerResponse
-				.ok()
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.body(service.findAll(), Producto.class);
+		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(service.findAll(), Producto.class);
 	}
-	
+
 	public Mono<ServerResponse> ver(ServerRequest request) {
 		String id = request.pathVariable("id");
 		return service.findById(id)
-				.flatMap(p -> ServerResponse
-						.ok()
-						.contentType(MediaType.APPLICATION_JSON_UTF8)
-						.body(fromObject(p)))//no es un tipo reactivo, es un objeto que se esta emitiendo
-						.switchIfEmpty(ServerResponse.notFound().build());
+				.flatMap(p -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(fromObject(p)))// no
+																													// es
+																													// un
+																													// tipo
+																													// reactivo,
+																													// es
+																													// un
+																													// objeto
+																													// que
+																													// se
+																													// esta
+																													// emitiendo
+				.switchIfEmpty(ServerResponse.notFound().build());
 	}
-	
+
 	public Mono<ServerResponse> crear(ServerRequest request) {
 		Mono<Producto> producto = request.bodyToMono(Producto.class);
-		
+
 		return producto.flatMap(p -> {
-			if(p.getCreateAt() == null) {
+			if (p.getCreateAt() == null) {
 				p.setCreateAt(new Date());
 			}
 			return service.save(p);
 		}).flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos".concat(p.getId())))
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.body(fromObject(p)));
+				.contentType(MediaType.APPLICATION_JSON_UTF8).body(fromObject(p)));
+	}
+
+	public Mono<ServerResponse> editar(ServerRequest request) {
+		Mono<Producto> producto = request.bodyToMono(Producto.class);
+		String id = request.pathVariable("id");
+		Mono<Producto> productoDb = service.findById(id);
+
+		return productoDb.zipWith(producto, (db, req) -> {
+			db.setNombre(req.getNombre());
+			db.setPrecio(req.getPrecio());
+			db.setCategoria(req.getCategoria());
+			return db;
+		}).flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON_UTF8).body(service.save(p), Producto.class))
+				.switchIfEmpty(ServerResponse.notFound().build());
 	}
 }
