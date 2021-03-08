73b7cefc9170b9f08eb08380150458fcb42c2937
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-03-08 21:21:04 +0100

    test para guardarUsuario correcto

--- Misc/spring-boot-gastos-compartidos/src/test/java/com/proyectosPersonales/springboot/app/service/usuario/UsuarioServiceImplTest.java
+++ Misc/spring-boot-gastos-compartidos/src/test/java/com/proyectosPersonales/springboot/app/service/usuario/UsuarioServiceImplTest.java
@@ -1,14 +1,18 @@
 package com.proyectosPersonales.springboot.app.service.usuario;
 
 import static com.proyectosPersonales.springboot.app.service.usuario.UsuarioServiceImplTestUtil.crearUsuarioCorrecto;
+import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.proyectosPersonales.springboot.app.gastos.dto.Usuario;
+import com.proyectosPersonales.springboot.app.gastos.repository.UsuarioDaoI;
 import com.proyectosPersonales.springboot.app.gastos.service.impl.UsuarioServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,9 +20,13 @@ public class UsuarioServiceImplTest {
 
     @InjectMocks
     private UsuarioServiceImpl usuarioService;
+    
+    @Mock
+    private UsuarioDaoI usuarioDao;
 
     @Test
     public void guardarUsuario_Correcto() throws Exception {
+    	when(usuarioDao.save(any())).thenReturn(crearUsuarioCorrecto());
         Usuario usuario = usuarioService.guardarUsuario(crearUsuarioCorrecto());
         assertNotNull(usuario);
     }
