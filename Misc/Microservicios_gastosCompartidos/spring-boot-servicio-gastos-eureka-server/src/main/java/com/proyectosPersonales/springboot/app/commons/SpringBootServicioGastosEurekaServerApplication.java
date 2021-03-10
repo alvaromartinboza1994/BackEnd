a5742605b40eabbc22a2c7d91d61534092910b63
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-03-10 12:58:23 +0100

    se añade servidor eureka

---
+++ Misc/Microservicios_gastosCompartidos/spring-boot-servicio-gastos-eureka-server/src/main/java/com/proyectosPersonales/springboot/app/commons/SpringBootServicioGastosEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.proyectosPersonales.springboot.app.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServicioGastosEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServicioGastosEurekaServerApplication.class, args);
+	}
+
+}
