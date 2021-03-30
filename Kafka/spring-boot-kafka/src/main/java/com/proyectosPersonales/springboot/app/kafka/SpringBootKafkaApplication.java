63187e28932ad9b2813923c8f62bb247c7cfaa48
alvaromartinboza1994 <alvaromartinboza.1994@gmail.com>
2021-03-30 09:18:57 +0200

    fin proyecto kafka

--- Kafka/spring-boot-kafka/src/main/java/com/proyectosPersonales/springboot/app/kafka/SpringBootKafkaApplication.java
+++ Kafka/spring-boot-kafka/src/main/java/com/proyectosPersonales/springboot/app/kafka/SpringBootKafkaApplication.java
@@ -1,13 +1,37 @@
 package com.proyectosPersonales.springboot.app.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class SpringBootKafkaApplication {
+public class SpringBootKafkaApplication implements ApplicationRunner {
+
+	// To publish a message, auto wire the Kafka Template object and produce the
+	// message
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKafkaApplication.class, args);
+	}
+
+	public void sendMessage(String msg) {
+		kafkaTemplate.send("mensaje", msg);
+	}
+
+	@KafkaListener(topics = "mensaje", groupId = "group-id")
+	public void listen(String message) {
+		System.out.println("Received Messasge in group - group-id: " + message);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		sendMessage("Hi Welcome to Spring For Apache Kafka");
 	}
 
 }
